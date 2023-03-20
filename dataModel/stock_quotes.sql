f768285ad85c4a00563f36bcefc9dea8b9e06475
Bartosz Dybowski <01161292@pw.edu.pl>
2023-03-20 03:07:52 +0100

    Update tables names and columns type in data model

--- dataModel/stock_quotes.sql
+++ dataModel/stock_quotes.sql
@@ -1,13 +1,17 @@
+CREATE DATABASE stock_quotes;
+
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
 CREATE TABLE instrument_thickers (
-    instrument_id BIGSERIAL NOT NULL PRIMARY KEY,
+    instrument_thicker_id UUID NOT NULL PRIMARY KEY DEFAULT uuid_generate_v4(),
     symbol VARCHAR(10) NOT NULL UNIQUE
 );
 
 CREATE TABLE transactions (
-    transaction_id BIGSERIAL NOT NULL PRIMARY KEY,
+    transaction_id UUID NOT NULL PRIMARY KEY DEFAULT uuid_generate_v4(),
     timestamp TIMESTAMPTZ NOT NULL,
-    instrument_id BIGINT NOT NULL REFERENCES instrument_thickers(instrument_id),
+    instrument_id UUID NOT NULL REFERENCES instrument_thickers(instrument_thicker_id),
     price NUMERIC(15, 2) NOT NULL CHECK (price >= 0)
 );
 
-CREATE INDEX CONCURRENTLY symbol_index ON instrument_thickers USING HASH(symbol);
+CREATE INDEX CONCURRENTLY symbol_hash_index ON instrument_thickers USING HASH(symbol);
